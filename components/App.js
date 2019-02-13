5f8e150baf5e21491b40f029bdab024392839df0
Quillor <info@quillor.com>
2019-02-12 23:36:12 -0500

    Save

--- components/App.js
+++ components/App.js
@@ -1,1 +1,10 @@
+import React, { Component } from 'react'
 
+class App extends Component {
+  render(){
+    return <div>This is React! </div>
+  }
+}
+
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,4 @@
+module.exports = {
+  entry: ['./client/c']
 
+}
